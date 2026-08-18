66188acc86bde650a978be7737b12c1627f44d18
Rage <owenmobley66@gmail.com>
2026-08-18 12:15:56 -0400

    ref: use string.Join to return valid image extensions

--- SunnyBot.Core/Commands/PhotoContestCommands.cs
+++ SunnyBot.Core/Commands/PhotoContestCommands.cs
@@ -27,7 +27,7 @@ public class PhotoContestCommands(SunnyBotDbContext db)
         {
             var err = new DiscordEmbedBuilder()
                 .WithTitle("Invalid File")
-                .WithDescription("Please attach a `.png`, `.jpeg`, or `.jpg` image.")
+                .WithDescription($"Please attach a {string.Join(", ", _validFileFormats.Select(f => $"`{f}`"))} image.")
                 .WithColor(new DiscordColor(0xFF0000));
             await ctx.RespondAsync(err);
             return;
